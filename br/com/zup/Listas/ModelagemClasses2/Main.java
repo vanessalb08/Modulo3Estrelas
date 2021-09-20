022597184a09dfbad1e932f67c8f8aee9c4c3a9b
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-20 14:19:18 -0300

    Resolução questão 1 lista modelagem de classes 20/09

---
+++ br/com/zup/Listas/ModelagemClasses2/Main.java
@@ -0,0 +1,4 @@
+package br.com.zup.Listas.ModelagemClasses2;
+
+public class Main {
+}
---
+++ br/com/zup/Listas/ModelagemClasses2/Questao1/Carro.java
@@ -0,0 +1,36 @@
+package br.com.zup.Listas.ModelagemClasses2.Questao1;
+
+public class Carro {
+    String nome;
+    String fabricante;
+    String cor;
+    double valor;
+    double peso;
+
+    public Carro(String nome, String fabricante,String cor, double valor, double peso){
+        this.nome = nome;
+        this.fabricante = fabricante;
+        this.cor = cor;
+        this.valor = valor;
+        this.peso = peso;
+    }
+
+    //Mostrar dados
+    public void mostrarDados(){
+        System.out.println("Nome: "+nome);
+        System.out.println("Fabricante: "+fabricante);
+        System.out.println("Cor: "+cor);
+        System.out.println("Valor: R$ "+valor);
+        System.out.println("Peso: " +peso+" kg");
+    }
+
+    //Desconto
+    public void darDesconto(double desconto){
+        valor -= desconto;
+    }
+
+    //Mudar cor do carro
+    public void pintar(String novaCor){
+        cor = novaCor;
+    }
+}
