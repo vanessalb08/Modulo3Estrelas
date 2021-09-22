58d545940ef535654ee9b66f6292e57b466ae0e0
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-22 09:51:21 -0300

    Inserindo mensagem aos comandos

--- br/com/zup/Listas/ListaEncapsulamento/LevelUp/Main.java
+++ br/com/zup/Listas/ListaEncapsulamento/LevelUp/Main.java
@@ -11,22 +11,34 @@ public class Main {
 
         //Aumentando volume 4x
         controle.aumentaVolume();
+        System.out.println("Novo volume: "+ controle.getVolume());
         controle.aumentaVolume();
+        System.out.println("Novo volume: "+ controle.getVolume());
         controle.aumentaVolume();
+        System.out.println("Novo volume: "+ controle.getVolume());
         controle.aumentaVolume();
         controle.mostraDados();
         //Mudando de canal 1 em 1 por 3x
         controle.aumentaCanal1();
+        System.out.println("Novo canal: "+ controle.getCanal());
         controle.aumentaCanal1();
+        System.out.println("Novo canal: "+ controle.getCanal());
         controle.diminuiCanal1();
+        System.out.println("Novo canal: "+ controle.getCanal());
         controle.aumentaCanal1();
+        System.out.println("Novo canal: "+ controle.getCanal());
         controle.mostraDados();
         //Mudando canal específico
         controle.trocaCanal(5);
+        System.out.println("Novo canal: "+ controle.getCanal());
         controle.diminuiVolume();
+        System.out.println("Novo volume: "+ controle.getVolume());
         controle.diminuiVolume();
+        System.out.println("Novo volume: "+ controle.getVolume());
         controle.diminuiVolume();
+        System.out.println("Novo volume: "+ controle.getVolume());
         controle.diminuiVolume();
+        System.out.println("Novo volume: "+ controle.getVolume());
         controle.mostraDados();
 
     }
