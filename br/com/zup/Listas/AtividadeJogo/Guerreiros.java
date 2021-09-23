14c6718f32660f20c2a5ff5e7de850391608171d
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-23 11:59:54 -0300

    Criando a classe Guerreiros e tornando-a subclasse da Heroi

---
+++ br/com/zup/Listas/AtividadeJogo/Guerreiros.java
@@ -0,0 +1,31 @@
+package br.com.zup.Listas.AtividadeJogo;
+
+public class Guerreiros extends Heroi {
+    private double poderAtaque;
+
+    //Construtores
+    public Guerreiros(){
+
+    }
+    public Guerreiros(double poderAtaque) {
+        this.poderAtaque = poderAtaque;
+    }
+
+    public Guerreiros(String nome, double vida, double poderAtaque) {
+        super(nome, vida);
+        this.poderAtaque = poderAtaque;
+    }
+
+    //Getters e Setters
+    public double getPoderAtaque() {
+        return poderAtaque;
+    }
+
+    public void setPoderAtaque(double poderAtaque) {
+        this.poderAtaque = poderAtaque;
+    }
+    //Métodos
+    private double aplicarDano(){
+        return aplicarDano();
+    }
+}
