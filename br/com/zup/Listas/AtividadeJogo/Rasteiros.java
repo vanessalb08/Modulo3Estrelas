3683ca1389c319cfe7ae297f73b3ec5c3d734116
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-23 14:24:00 -0300

    Criando a classe Rasteiros e tornando-a sublcasse da Inimigos

---
+++ br/com/zup/Listas/AtividadeJogo/Rasteiros.java
@@ -0,0 +1,32 @@
+package br.com.zup.Listas.AtividadeJogo;
+
+public class Rasteiros extends Inimigos {
+    private double danoEspinhos;
+
+    //Construtores
+    public Rasteiros(){
+
+    }
+    public Rasteiros(double danoEspinhos) {
+        this.danoEspinhos = danoEspinhos;
+    }
+
+    public Rasteiros(String nome, double vida, double danoEspinhos) {
+        super(nome, vida);
+        this.danoEspinhos = danoEspinhos;
+    }
+    //Getters e Setters
+
+    public double getDanoEspinhos() {
+        return danoEspinhos;
+    }
+
+    public void setDanoEspinhos(double danoEspinhos) {
+        this.danoEspinhos = danoEspinhos;
+    }
+
+    //Métodos
+    private double aplicarDados(){
+        return aplicarDados();
+    }
+}
