2d6009a05055cb6efc17f1a4f8d54a3be7a454f4
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-23 11:56:20 -0300

    Criando a classe Magos e tornando ela subclasse da classe héroi

---
+++ br/com/zup/Listas/AtividadeJogo/Magos.java
@@ -0,0 +1,32 @@
+package br.com.zup.Listas.AtividadeJogo;
+
+public class Magos extends Heroi{
+    private double poderMagico;
+
+    //Construtores
+    public Magos (){
+
+    }
+    public Magos(double poderMagico) {
+        this.poderMagico = poderMagico;
+    }
+
+    public Magos(String nome, double vida, double poderMagico) {
+        super(nome, vida);
+        this.poderMagico = poderMagico;
+    }
+
+    //Getters e Setters
+
+    public double getPoderMagico() {
+        return poderMagico;
+    }
+
+    public void setPoderMagico(double poderMagico) {
+        this.poderMagico = poderMagico;
+    }
+    //Métodos
+    private double aplicarMagia(){
+        return aplicarMagia();
+    }
+}
