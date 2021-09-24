624c928a24910191abcc26b71ab4129e83b16d04
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-24 15:47:56 -0300

    Criando as classes e seus atributos

--- br/com/zup/ListasDeExercicios/ListaClasses2/Coordenadores.java
+++ br/com/zup/ListasDeExercicios/ListaClasses2/Coordenadores.java
@@ -1,4 +1,20 @@
 package br.com.zup.ListasDeExercicios.ListaClasses2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coordenadores {
+    private List<Professores> professores = new ArrayList<>();
+
+    public Coordenadores(List<Professores> professores) {
+        this.professores = professores;
+    }
+
+    public List<Professores> getProfessores() {
+        return professores;
+    }
+
+    public void setProfessores(List<Professores> professores) {
+        this.professores = professores;
+    }
 }
