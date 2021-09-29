5ad00ed613f9547d3a611e0f31fd3384d212e61d
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-29 08:16:22 -0300

    Criar menu classe sistema

---
+++ br/com/zup/ListasDeExercicios/Restaurante/Sistema.java
@@ -0,0 +1,15 @@
+package br.com.zup.ListasDeExercicios.Restaurante;
+
+import java.util.Scanner;
+
+public class Sistema {
+    //Menu
+    public static void menu(){
+        System.out.println("Bem vindo ao Los pollos hermanos");
+        System.out.println("Segue opções: ");
+        System.out.println("1 - Cadastrar novo prato");
+        System.out.println("2 - Mostrar cardápio");
+        System.out.println("3 - Sair do programa");
+    }
+
+}
