da78552c83fb5f6d9ac32bf0790b64381f799d47
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-28 23:07:31 -0300

    Criar classe Pratos

---
+++ br/com/zup/ListasDeExercicios/Restaurante/Pratos.java
@@ -0,0 +1,38 @@
+package br.com.zup.ListasDeExercicios.Restaurante;
+
+import java.util.List;
+
+public class Pratos {
+    private String nomePrato;
+    private double valorPrato;
+    private List<Ingredientes> ingredientes;
+
+    //Contrutores
+
+    public Pratos(String nomePrato, double valorPrato) {
+        this.nomePrato = nomePrato;
+        this.valorPrato = valorPrato;
+    }
+    //Getters e Setters
+
+    public String getNomePrato() {
+        return nomePrato;
+    }
+
+    public void setNomePrato(String nomePrato) {
+        this.nomePrato = nomePrato;
+    }
+
+    public double getValorPrato() {
+        return valorPrato;
+    }
+
+    public void setValorPrato(double valorPrato) {
+        this.valorPrato = valorPrato;
+    }
+
+    public List<Ingredientes> getIngredientes() {
+        return ingredientes;
+    }
+
+}
