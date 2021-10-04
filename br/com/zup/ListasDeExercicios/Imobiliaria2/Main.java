750c9f95fb3711ea6e7f0abfaeb030b0423392a0
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-10-04 15:33:30 -0300

    Testar método cadastrarImovel

--- br/com/zup/ListasDeExercicios/Imobiliaria2/Main.java
+++ br/com/zup/ListasDeExercicios/Imobiliaria2/Main.java
@@ -4,10 +4,21 @@ public class Main {
     public static void main(String[] args) {
 
         Funcionario func1 = new Funcionario("Andre","0314083","312-b");
+        Funcionario func2 = new Funcionario("Vinicius","31431414","123-c");
+
         Morador morador1 = new Morador("Vanessa","02349423432",1500);
+        Morador morador2 = new Morador("Everton","30981314",1000);
+
         Imovel imovel1 = new Imovel(func1,"Rua J",1000);
+        Imovel imovel2 = new Imovel(func2,"Av H",800);
+
+        Imobiliaria imobiliaria = new Imobiliaria();
 
         imovel1.cadastrarMoradores(morador1);
-        System.out.println(imovel1);
+        imobiliaria.cadastrarImovel(imovel1);
+        System.out.println(imobiliaria);
+        imovel2.cadastrarMoradores(morador2);
+        imobiliaria.cadastrarImovel(imovel2);
+        System.out.println(imobiliaria);
     }
 }
