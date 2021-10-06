a995c909c462bdff23713553f81dda19020e390c
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-10-06 14:25:21 -0300

    Criar lista de faturas

--- br/com/zup/Aula5POOEnum/ServicoFatura.java
+++ br/com/zup/Aula5POOEnum/ServicoFatura.java
@@ -1,4 +1,9 @@
 package br.com.zup.Aula5POOEnum;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicoFatura {
+    private static List<Fatura> faturas = new ArrayList<>();
+
 }
