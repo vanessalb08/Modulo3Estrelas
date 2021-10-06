6f7e3dac805832208c46273abd7a45e37190162c
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-10-06 14:29:43 -0300

    Criar métodos menu e capturar Dados

--- br/com/zup/Aula5POOEnum/Sistema.java
+++ br/com/zup/Aula5POOEnum/Sistema.java
@@ -1,4 +1,18 @@
 package br.com.zup.Aula5POOEnum;
 
+import java.util.Scanner;
+
 public class Sistema {
+
+    //Linha de frente com o usuário
+
+    private static Scanner capturarDados(String mensagem){
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
+    public static void menu(){
+        System.out.println("[1] - Cadastrar Consumidor");
+        System.out.println("[2] - Cadastrar fatura");
+        System.out.println("[3] - Sair");
+    }
 }
