8073f56ba9ae0492f258728e043fa2a1d5f2c235
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-10-08 14:13:04 -0300

    Criar classe Vendedor

---
+++ br/com/zup/DesafioModulo3/Vendedor.java
@@ -0,0 +1,30 @@
+package br.com.zup.DesafioModulo3;
+
+public class Vendedor extends Pessoa{
+    private String ctps;
+
+    public Vendedor() {
+
+    }
+
+    public Vendedor(String nome, String cpf, String email, String ctps) {
+        super(nome, cpf, email);
+        this.ctps = ctps;
+    }
+
+    public String getCtps() {
+        return ctps;
+    }
+
+    public void setCtps(String ctps) {
+        this.ctps = ctps;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retornaDados = new StringBuilder();
+        retornaDados.append(super.toString());
+        retornaDados.append("CTPS: "+ ctps);
+        return retornaDados.toString();
+    }
+}
